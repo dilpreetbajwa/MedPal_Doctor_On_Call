c6b8778fa5bb19f4b988c6afac5e9f61a5dc74b7
anithjoy <anithjoy1@gmail.com>
2024-09-05 23:36:05 -0400

    Edit schedule time - frontend

--- src/components/UI/form/TimePicer.jsx
+++ src/components/UI/form/TimePicer.jsx
@@ -1,15 +1,16 @@
-import moment from 'moment';
-import { TimePicker } from 'antd';
+import React, { useState } from "react";
+import moment from "moment";
+import { TimePicker } from "antd";
 
-const TimePicer = ({ id, time, handleFunction, showValue = false }) => {
-    const defaultTime = moment(time, 'h:mm a')
-    return (
-        <TimePicker
-            defaultValue={showValue && defaultTime}
-            use12Hours
-            format="h:mm a"
-            onChange={(time, timeString) => handleFunction(id, time, timeString)}
-        />
-    )
+const TimePicer = ({ id, time, handleFunction, showValue = true }) => {
+  const defaultTime = moment(time, "h:mm a");
+  return (
+    <TimePicker
+      value={showValue && defaultTime}
+      use12Hours
+      format="h:mm a"
+      onChange={(time, timeString) => handleFunction(id, time, timeString)}
+    />
+  );
 };
 export default TimePicer;
