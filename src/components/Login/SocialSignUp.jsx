dc4df4c6c64758a742f0d323fe2e85c3237057b3
anithjoy <anithjoy1@gmail.com>
2024-10-22 13:49:34 -0400

    10/22/2024 1:49PM

--- src/components/Login/SocialSignUp.jsx
+++ src/components/Login/SocialSignUp.jsx
@@ -1,10 +1,29 @@
 import React, { useState } from "react";
 import { FaFacebook, FaApple, FaGoogle } from "react-icons/fa";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "./firebase";
 
 const SocialSignUp = () => {
   const [error] = useState({});
-  const handleGoogleSignIn = () => {
-    // changes
+  const handleGoogleSignIn = async () => {
+    // changes to be done. Error facing has request action is invalid
+    console.log("Google sign in running ", auth);
+    const provide = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provide);
+
+      console.log(result);
+    } catch (error) {
+      console.error("Error during google sign in->", error);
+    }
+  };
+
+  const handleFacebookSignIn = () => {
+    console.log("Facebook Sign In");
+  };
+
+  const handleAppleSignIn = () => {
+    console.log("Apple Sign In");
   };
 
   return (
@@ -13,10 +32,10 @@ const SocialSignUp = () => {
         <div className="social-icon" onClick={handleGoogleSignIn}>
           <FaGoogle />
         </div>
-        <div className="social-icon">
+        <div className="social-icon" onClick={handleFacebookSignIn}>
           <FaFacebook />
         </div>
-        <div className="social-icon">
+        <div className="social-icon" onClick={handleAppleSignIn}>
           <FaApple />
         </div>
       </div>
