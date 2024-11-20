42b2f4fec8900bded67225a1f3d77a670245c2e0
dakShh <daksh.dk207@gmail.com>
2024-11-20 10:46:17 -0500

    Mapping sidebar list items

--- src/components/UI/DashboardSidebar.jsx
+++ src/components/UI/DashboardSidebar.jsx
@@ -17,146 +17,118 @@ import {
 } from 'react-icons/fa';
 
 const DashboardSidebar = () => {
-    const { data, role } = useAuthCheck();
+    const { data: authData, role } = useAuthCheck();
 
+    const patient_sidebar_menu = [
+        {
+            link: '/dashboard',
+            name: 'Dashboard',
+            icon: <FaTable className="icon" />,
+        },
+        {
+            link: '/dashboard/profile-setting',
+            name: 'Profile Setting',
+            icon: <FaUserCog className="icon" />,
+        },
+        {
+            link: '/dashboard/change-password',
+            name: 'Change Password',
+            icon: <FaLock className="icon" />,
+        },
+        {
+            link: '/',
+            name: 'Logout',
+            icon: <FaSignOutAlt className="icon" />,
+        },
+    ];
+
+    const doctor_menu_item = [
+        {
+            link: '/dashboard',
+            name: 'Dashboard',
+            icon: <FaTable className="icon" />,
+        },
+        {
+            link: '/dashboard/appointments',
+            name: 'Appointments',
+            icon: <FaCalendarDay className="icon" />,
+        },
+        {
+            link: '/dashboard/my-patients',
+            name: 'My Patients',
+            icon: <FaUserInjured className="icon" />,
+        },
+        {
+            link: '/dashboard/prescription',
+            name: 'Prescription',
+            icon: <FaUserInjured className="icon" />,
+        },
+        {
+            link: '/dashboard/schedule',
+            name: 'Schedule Timings',
+            icon: <FaCalendarDay className="icon" />,
+        },
+
+        {
+            link: '/dashboard/profile-setting',
+            name: 'Profile Settings',
+            icon: <FaUserCog className="icon" />,
+        },
+        {
+            link: '/dashboard/change-password',
+            name: 'Change Password',
+            icon: <FaLock className="icon" />,
+        },
+        {
+            link: '/',
+            name: 'Logout',
+            icon: <FaSignOutAlt className="icon" />,
+        },
+    ];
     return (
         <div className="profile-sidebar p-3 rounded">
             <div className="p-2 text-center border-bottom">
-                {role === 'doctor' ? (
-                    <div className="profile-info text-center">
-                        <Link to={'/'}>
-                            <img src={data?.img ? data?.img : img} alt="" />
-                        </Link>
-                        <div className="profile-details">
-                            <h5 className="mb-0">{data?.firstName + ' ' + data?.lastName}</h5>
-                            <div>
-                                <p className="mb-0">{data?.designation}</p>
-                            </div>
+                <div className="profile-info text-center">
+                    <Link to={'/'}>
+                        <img src={authData?.img ? authData?.img : img} alt="" />
+                    </Link>
+                    <div className="profile-details">
+                        <h5 className="mb-0">{authData?.firstName + ' ' + authData?.lastName}</h5>
+                        <div>
+                            {role === 'doctor' && (
+                                <p className="mb-0">{authData?.designation ?? 'Designation'}</p>
+                            )}
+                            <p className="mb-0">{authData?.email ?? 'Designation'}</p>
                         </div>
                     </div>
-                ) : (
-                    <div className="profile-info text-center">
-                        <Link to={'/'}>
-                            <img src={data?.img ? data?.img : img} alt="" />
-                        </Link>
-                        <div className="profile-details">
-                            <h5 className="mb-0">{data?.firstName + ' ' + data?.lastName}</h5>
-                            <div className="mt-2">
-                                {/* <p className=' form-text m-0'>24 Jul 1983, 38 Years</p>
-                                    <p className=' form-text m-0'> New Yourk , USA</p> */}
-                                <p className=" form-text m-0">{data?.email}</p>
-                            </div>
-                        </div>
-                    </div>
-                )}
+                </div>
             </div>
             <nav className="dashboard-menu">
                 {role === 'patient' ? (
                     <ul>
-                        <li>
-                            <NavLink to={'/dashboard'} activeClassName="active" end>
-                                <FaTable className="icon" />
-                                <span>Dashboard</span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/dashboard/favourite'} activeClassName="active">
-                                <FaHouseUser className="icon" />
-                                <span>Favourites</span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/dashboard/profile-setting'} activeClassName="active">
-                                <FaUserCog className="icon" />
-                                <span>Profile Settings</span>
-                            </NavLink>
-                        </li>
-
-                        <li>
-                            <NavLink to={'/dashboard/change-password'} activeClassName="active">
-                                <FaLock className="icon" />
-                                <span>Change Password</span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/'}>
-                                <FaSignOutAlt className="icon" />
-                                <span>Logout</span>
-                            </NavLink>
-                        </li>
+                        {patient_sidebar_menu?.map((menu, index) => {
+                            return (
+                                <li>
+                                    <NavLink to={menu.link} activeClassName="active" end>
+                                        {menu.icon}
+                                        <span>{menu.name}</span>
+                                    </NavLink>
+                                </li>
+                            );
+                        })}
                     </ul>
                 ) : (
                     <ul>
-                        <li>
-                            <NavLink to={'/dashboard'} activeClassName="active" end>
-                                <FaTable className="icon" />
-                                <span>Dashboard</span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/dashboard/appointments'} activeClassName="active" end>
-                                <FaCalendarDay className="icon" />
-                                <span>Appointments</span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/dashboard/my-patients'} activeClassName="active" end>
-                                <FaUserInjured className="icon" />
-                                <span>My Patients</span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/dashboard/prescription'} activeClassName="active" end>
-                                <FaUserInjured className="icon" />
-                                <span>Prescription</span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/dashboard/schedule'} activeClassName="active" end>
-                                <FaCalendarDay className="icon" />
-                                <span>Schedule Timings</span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/dashboard/invoices'} activeClassName="active" end>
-                                <FaHourglassStart className="icon" />
-                                <span>Invoices</span>
-                            </NavLink>
-                        </li>
-
-                        <li>
-                            <NavLink to={'/dashboard/reviews'} activeClassName="active" end>
-                                <FaRegStar className="icon" />
-                                <span>Reviews</span>
-                            </NavLink>
-                        </li>
-
-                        <li>
-                            <NavLink to={'/dashboard/profile-setting'} activeClassName="active" end>
-                                <FaUserCog className="icon" />
-                                <span>Profile Settings</span>
-                            </NavLink>
-                        </li>
-
-                        <li>
-                            <NavLink to={'/dashboard/blogs'} activeClassName="active" end>
-                                <FaBlog className="icon" />
-                                <span>Blogs (Will move to Admin)</span>
-                            </NavLink>
-                        </li>
-
-                        <li>
-                            <NavLink to={'/dashboard/change-password'} activeClassName="active" end>
-                                <FaLock className="icon" />
-                                <span>Change Password</span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/'}>
-                                <FaSignOutAlt className="icon" end />
-                                <span>Logout</span>
-                            </NavLink>
-                        </li>
+                        {doctor_menu_item?.map((menu, index) => {
+                            return (
+                                <li>
+                                    <NavLink to={menu.link} activeClassName="active" end>
+                                        {menu.icon}
+                                        <span>{menu.name}</span>
+                                    </NavLink>
+                                </li>
+                            );
+                        })}
                     </ul>
                 )}
             </nav>
